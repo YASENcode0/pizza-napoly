cdb291e4ceb2300b286a93da90b520027c6f38e0
YASENcode0 <easeneaseneasen123@gmail.com>
2024-12-16 18:07:48 +0200

    fix message ui , animation and bugs

--- client/src/components/Message/Message.jsx
+++ client/src/components/Message/Message.jsx
@@ -2,27 +2,35 @@ import React, { useEffect, useState } from "react";
 import "./Message.css";
 import { IoMdClose } from "react-icons/io";
 
-export default function Message({ content }) {
-   const [on, setOn] = useState(true);
-   useEffect(() => {
-      setTimeout(() => {
-         setOn(false);
-      }, 4000);
-   }, []);
+export default function Message({ content , type }) {
+  const [on, setOn] = useState(true);
+  const [close, setClose] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setOn(false);
+      closeMessage();
+    }, 4000);
+  }, []);
 
-   if (on) {
-      return (
-         <div className={`message-unit ${!on && "hide-message"} `}>
-            <button
-               onClick={() => {
-                  setOn(false);
-               }}
-            >
-               <IoMdClose />
-            </button>
-            {content}
-            <hr style={{ animationPlayState: `${on && "running"}` }} />
-         </div>
-      );
-   }
+  function closeMessage() {
+    setTimeout(() => {
+      setClose(false);
+    }, [1000]);
+  }
+
+  if (close) {
+    return (
+      <div className={`message-unit ${!on && "hide-message"} ${type ? "message-note" : 'message-warning'}`}>
+        <button
+          onClick={() => {
+            setOn(false);
+          }}
+        >
+          <IoMdClose />
+        </button>
+        {content}
+        <hr style={{ animationPlayState: `${on && "running"}` }} />
+      </div>
+    );
+  }
 }
